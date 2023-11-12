a5d699c0d41c75a20e2e9e2624f34154e8cd0e3d
Michael Kinder <me@mwk.email>
2023-11-12 17:16:31 -0500

    add docs

---
+++ public/admin-docs/3.1/_static/js/footer.js
@@ -0,0 +1,27 @@
+document.write('<div class="rst-versions" data-toggle="rst-versions" role="note" aria-label="versions">\
+   <span class="rst-current-version" data-toggle="rst-current-version">\
+     <span class="fa fa-book"> Singularity Docs - Admin Guide </span>\
+     v: latest\
+     <span class="fa fa-caret-down"></span>\
+   </span>\
+   <div class="rst-other-versions">\
+     <dl>\
+       <dt>Versions</dt>\
+         <dd><a href="https://www.sylabs.io/guides/3.0/admin-guide/">v3.0</a></dd>\
+     </dl>\
+     <dl>\
+       <dt>Downloads</dt>\
+       <dd>\
+         <a href="https://www.sylabs.io/guides/3.0/admin-guide.pdf">v3.0 PDF</a>\
+       </dd>\
+     </dl>\
+     <dl>\
+       <dt>Do you want to contribute? </dt>\
+         <dd>\
+           <a href="https://github.com/sylabs/singularity-admindocs/tree/3.0">Find us on GitHub!</a>\
+         </dd>\
+     </dl>\
+\
+   </div>\
+ </div>\
+');
